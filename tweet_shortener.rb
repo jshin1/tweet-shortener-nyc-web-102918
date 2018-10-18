07dbfd0f9590bf861c9759d2f860ca2afe419880
Ji S <j2yshin@gmail.com>
2018-10-18 20:32:47 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -41,7 +41,7 @@ def selective_tweet_shortener(tweet)
 end
 
 def shortened_tweet_truncator(tweet)
-  if word_substituter(tweet) > 140
+  if word_substituter(tweet).length > 140
     tweet[0..140]
   end
 end
